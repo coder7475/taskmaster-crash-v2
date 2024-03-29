6a6d1497c58abad43636838c17b6278d50877d0e
coder7475 <robiulhossain7475@gmail.com>
2024-03-29 23:02:17 +0600

    feat: dispatch setUser

--- src/components/layouts/PrivateRoute.jsx
+++ src/components/layouts/PrivateRoute.jsx
@@ -1,18 +1,24 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import Loading from './Loading';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import auth from '../../utils/firebase.config';
 import { onAuthStateChanged } from 'firebase/auth';
+import { setUser } from '../../redux/features/user/userSlice';
 
 const PrivateRoute = ({ children }) => {
   const { pathname } = useLocation();
-
+  const dispatch = useDispatch();
   const { email, isLoading } = useSelector((state) => state.userSlice);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      if (user) {
+        dispatch(setUser({
+          name: user.displayName,
+          email: user.email,
+        }))
+      }
     })
   }) 
   if (isLoading) {
--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -59,4 +59,5 @@ const userSlice = createSlice({
   }
 });
 
+export const { setUser } = userSlice.actions;
 export default userSlice.reducer;
