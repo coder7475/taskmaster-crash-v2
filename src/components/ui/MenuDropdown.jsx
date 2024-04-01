bfbd4bc91db2baf0f8672dfc349ba5b11a01f8bb
coder7475 <robiulhossain7475@gmail.com>
2024-04-02 04:10:39 +0600

    feat: attach logout function to button

--- src/components/ui/MenuDropdown.jsx
+++ src/components/ui/MenuDropdown.jsx
@@ -49,6 +49,7 @@ export default function MenuDropdown({ children }) {
             <Menu.Item>
               {({ active }) => (
                 <button
+                onClick={logout}
                   className={`${
                     active ? 'bg-primary text-white' : 'text-gray-900'
                   } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
