6a14898135ac6807867d080370a0023d0c2448a1
coder7475 <robiulhossain7475@gmail.com>
2024-04-07 22:42:54 +0600

    feat: connect base api to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksSlice from './features/tasks/tasksSlice';
 import userSlice from './features/user/userSlice';
+import baseApi from './features/api/baseApi';
 
 const store = configureStore({
   reducer: {
     tasksSlice: tasksSlice,
     userSlice: userSlice,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
 });
 
